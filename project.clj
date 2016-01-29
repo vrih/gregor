3c83bd4939eede222c1563c99a9fcc9bb6e3a2e2
Cody Canning <cocanning11@gmail.com>
2016-01-29 16:54:30 -0500

    add tests and dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
 (defproject gregor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "See Readme"
+  :url "https://github.com/weftio/gregor.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.apache.kafka/kafka_2.10 "0.9.0.0"]
+                 [environ "1.0.1"]
+                 [com.taoensso/timbre "4.0.2"]
+                 [prismatic/schema "0.4.3"]]
   :main ^:skip-aot gregor.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- test/gregor/core_test.clj
+++ test/gregor/core_test.clj
@@ -1,7 +1,37 @@
 (ns gregor.core-test
-  (:require [clojure.test :refer :all]
-            [gregor.core :refer :all]))
+  (:require [gregor.core :refer :all]
+            [clojure.test :refer [is testing deftest]])
+  (:import [org.apache.kafka.clients.consumer MockConsumer OffsetResetStrategy ConsumerRecords ConsumerRecord]
+           [org.apache.kafka.clients.producer MockProducer]
+           [org.apache.kafka.common TopicPartition]
+           [org.apache.kafka.common.serialization StringSerializer]
+           [java.util ArrayList]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(deftest producing
+  (testing "record->map and send-message")
+  (let [p (MockProducer. true (StringSerializer.) (StringSerializer.))]
+    (send-message p "unittest" {:a 1 :b "two"})
+    (let [values (.history p)]
+      (is (= {:a 1 :b "two"}
+             (-> values
+                 (first)
+                 (record->map)
+                 (:value))))
+      (.close p))))
+
+(deftest consuming
+  (testing "messages-seq")
+  (let [topics ["unittest"]
+        tp (TopicPartition. "unittest" 0)
+        c (doto (MockConsumer. (OffsetResetStrategy/EARLIEST))
+            (.assign [tp])
+            (.updateBeginningOffsets {(TopicPartition. "unittest" 0) 0})
+            (.addRecord (ConsumerRecord. "unittest" 0 0 0 {:a 1}))
+            (.addRecord (ConsumerRecord. "unittest" 0 0 0 {:b 2})))
+        ms (messages-seq c)]
+    (is (= {:a 1}
+           (-> ms (first) (first) (:value))))
+    (is (= {:b 2}
+           (-> ms (first) (second) (:value))))))
+
