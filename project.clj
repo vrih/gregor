44abfdf7b7d1d33fef2c301a457debc53dfa4480
Cody Canning <cocanning11@gmail.com>
2016-01-30 13:51:37 -0500

    move deps

--- project.clj
+++ project.clj
@@ -6,8 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.apache.kafka/kafka_2.10 "0.9.0.0"]
-                 [environ "1.0.1"]
-                 [com.taoensso/timbre "4.0.2"]
                  [prismatic/schema "0.4.3"]]
   :main ^:skip-aot gregor.core
   :target-path "target/%s"
