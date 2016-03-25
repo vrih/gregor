ffd0a209e1ca8ccf4c304d96889c31e7de63faba
Cody Canning <cocanning11@gmail.com>
2016-03-25 14:28:14 -0400

    rm core.async

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]
                  [org.apache.kafka/kafka_2.10 "0.9.0.1"]
                  [prismatic/schema "0.4.3"]]
   :main ^:skip-aot gregor.core
