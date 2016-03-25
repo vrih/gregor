1fc35e72c4fc9c3aec6d3ac042cb5971390df62b
Cody Canning <cocanning11@gmail.com>
2016-03-25 17:25:12 -0400

    rm schema dep

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.apache.kafka/kafka_2.10 "0.9.0.1"]
-                 [prismatic/schema "1.0.5"]]
+                 [org.apache.kafka/kafka_2.10 "0.9.0.1"]]
   :plugins [[s3-wagon-private "1.1.2"]
             [lein-codox "0.9.3"]])
